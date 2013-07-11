d95e136d4a0c955c01fded2e6ea806e1c14bf4dc
Jakob Schöttl <jakob.schoettl@siemens.com>
2013-07-11 14:25:37 +0200

    Extracted connector php code

--- src/connectors/GsFileManager.php
+++ src/connectors/GsFileManager.php
@@ -780,20 +780,3 @@ class GSFileManager {
         return $String;
     }
 }
-
-set_time_limit(0);
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-mb_internal_encoding("UTF-8");
-
-$options = array();
-$options['max_upload_filesize'] = '2000'; //(the size in Kbytes)
-$options[GSFileManager::$root_param] = 'C:/temp';
-$manager = new GSFileManager(new GSFileSystemFileStorage(), $options);
-try {
-    $result = $manager->process($_REQUEST);
-} catch (Exception $e) {
-    $result = '{result: \'0\', gserror: \''.addslashes($e->getMessage()).'\', code: \''.$e->getCode().'\'}';
-}
-echo $result;
-?>
