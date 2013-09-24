b82ddf15cc03a006b1f0bd54b4b470362bea3dd0
Jakob Schöttl <jakob.schoettl@siemens.com>
2013-09-24 17:51:58 +0200

    Removed deploy tools

--- tools/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find "$1" -mindepth 1 -maxdepth 1 -not -name .git\* -not -name tools   -exec  cp -ruf "{}" "$2" \;
-#'xcopy "{}" "'$2'" /y /q /s /l /d' \;
--- tools/deploy_local.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./deploy.sh ".." "c:/xampp/htdocs/gsfilemanager"
